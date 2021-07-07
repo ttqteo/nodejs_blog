b3c9d4001528a6933d8886ec612159c168a4d438
Tran Tu Quang <tuquangtran123@gmail.com>
2021-07-07 23:43:11 +0700

    add npm-morgan, express-handlebars, npm-sass to convert scss to css

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const handlebars  = require('express-handlebars')
+const app = express()
+const port = 3000
+
+// Use static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// HTTP logger
+app.use(morgan('combined'))
+
+// Template engine
+app.engine('hbs', handlebars({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'))
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.get('/blog', (req, res) => {
+  res.render('blog')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
